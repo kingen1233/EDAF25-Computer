e07a5bb516baf51b5b31ca43ae6df946beb60a45
André <Andr@DESKTOP-RBNSL03>
2020-04-02 15:06:18 +0200

    Probably finished, not 100% sure

--- EDAF25-Computer/src/computer/Computer.java
+++ EDAF25-Computer/src/computer/Computer.java
@@ -1,9 +1,27 @@
 package computer;
 
+import data.*;
+import instructions.*;
+
 public class Computer {
 
-	public Computer() {
-		// TODO Auto-generated constructor stub
+	private Memory memory;
+	private Program program;
+
+	public Computer(Memory memory) {
+
+		this.memory = memory;
+
+	}
+	
+	public void load(Program program) {
+		
+		this.program = program;
+		
 	}
 
+	public void run() {
+		
+		program.run(memory);
+	}
 }
