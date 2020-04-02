c1b77e66a9b4980127a6056810a4a203589fa638
Linus <Linus@LAPTOP-V8FT09IM.lan>
2020-04-02 21:32:59 +0200

    sluta knapra

--- EDAF25-Computer/src/data/Memory.java
+++ EDAF25-Computer/src/data/Memory.java
@@ -3,7 +3,24 @@ package data;
 public abstract class Memory<T> {
 
 	public Memory() {
-		// TODO Auto-generated constructor stub
+		
+		private T[] memory;
+		
+		protected Memory(Word[] mem){
+			memory = mem;
+		}
+		
+		
+	    public void setWord(Address a, Word word) {
+	        memory[a.DATA] = word;
+	    }
+
+	    public Word getWord(Address a) {
+	        return memory[a.DATA];
+	    }
+		
+		
+		
 	}
 
 }
--- EDAF25-Computer/src/data/Word.java
+++ EDAF25-Computer/src/data/Word.java
@@ -1,9 +1,14 @@
 package data;
 
 public abstract class Word<T> extends Data {
+	
+	private T word;
 
 	public Word(T word) {
+		this.word = word;
 		super(word);
 	}
+	
+	
 
 }
